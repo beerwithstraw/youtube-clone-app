b6dae79667e0c9b8f4a416ab3d98f9755ee7ed14
beerwithstraw <agarwalp@tcd.ie>
2020-11-13 02:37:13 +0000

    Basic Structure and API Call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Grid } from '@material-ui/core';
+
+import youtube from './api/youtube';
+
+class App extends React.Component{
+  render(){
+    return(
+      <Grid justify="center"  container spacing={16}>
+        <Grid item xs={12}>
+          <Grid container spacing={16}>
+            <Grid item xs={12}>
+              <h1>Search Bar</h1>
+            </Grid>
+            <Grid item xs={8}>
+              <h1>Playing Video</h1>
+            </Grid>
+            <Grid item xs={4}>
+              <h1>Recommended</h1>
+            </Grid>                        
+          </Grid>
+        </Grid>
+      </Grid>
+
+
+
+    )
+  }
 }
-
 export default App;
